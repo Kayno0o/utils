f7ce870204eeb5f69a116fd11ab0e32606e52245
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-11-06 10:54:28 +0100

    feat: add hslToHex and colorFromString

--- src/colorUtils.ts
+++ src/colorUtils.ts
@@ -29,6 +29,17 @@ export function rgbToHex(rgb: [number, number, number] | number, g?: number, b?:
   return `#${((1 << 24) + (rgb << 16) + (g! << 8) + b!).toString(16).slice(1)}`.toUpperCase()
 }
 
+export function hslToHex(h: number, s: number, l: number) {
+  l /= 100
+  const a = s * Math.min(l, 1 - l) / 100
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+    return Math.round(255 * color).toString(16).padStart(2, '0') // Convert to hex
+  }
+  return `#${f(0)}${f(8)}${f(4)}`
+}
+
 export function randomHex(isCrypto = false) {
   return `#${Math.floor(randomInt(0, 16777215, isCrypto)).toString(16).padStart(6, '0')}`.toUpperCase()
 }
@@ -42,4 +53,16 @@ export function opacityColor(hex1: string, hex2: string, a1: number) {
   const b3 = b2 + (b1 - b2) * a1
 
   return rgbToHex(r3, g3, b3)
+}
+
+export function colorFromString(str: string, { hueDecay = 0, l = 65, sRange = [40, 60] }: { hueDecay: number, l: number, sRange: [number, number] }) {
+  let hash = 0
+  for (const char of str) {
+    hash ^= char.charCodeAt(0) + (hash << 5) + (hash >> 2)
+  }
+
+  const h = (hash + hueDecay) % 360 // hue
+  const s = sRange[0] + (hash % (sRange[1] - sRange[0])) // saturation between range
+
+  return hslToHex(h, s, l)
 }
