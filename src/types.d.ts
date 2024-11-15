b403bc2c3848a746ca80391b73b77be438449552
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-11-15 09:58:57 +0100

    fix: export type IntRange

--- src/types.d.ts
+++ src/types.d.ts
@@ -30,4 +30,4 @@ type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] exte
   ? Acc[number]
   : Enumerate<N, [...Acc, Acc['length']]>
 
-type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
+export type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
