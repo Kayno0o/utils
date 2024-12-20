6d7d28569ba785343bea47cf089e5cc6ab7890e7
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-12-20 23:00:20 +0100

    fix: logger example

--- examples/logger.ts
+++ examples/logger.ts
@@ -1,9 +1,8 @@
 import type { LogLevel } from '../index'
-import path from 'node:path'
 import chalk from 'chalk'
 import { declareLogger } from '../index'
 
-const { log } = declareLogger<'discord' | 'sqlite' | 'rcon'>({ logFilePath: path.resolve('latest.log'), logLevel: import.meta.env.LOG_LEVEL as LogLevel | undefined, serviceColor: {
+const log = declareLogger<'discord' | 'sqlite' | 'rcon'>({ logLevel: import.meta.env.LOG_LEVEL as LogLevel | undefined, serviceColor: {
   discord: chalk.bold.yellow,
   rcon: chalk.bold.blue,
   sqlite: chalk.bold.magenta,
@@ -11,10 +10,10 @@ const { log } = declareLogger<'discord' | 'sqlite' | 'rcon'>({ logFilePath: path
 
 log('error', 'rcon', 'type', 'messages')
 
-const { log: logWithoutService } = declareLogger({})
+const logWithoutService = declareLogger({})
 logWithoutService('success', 'type', 'messages')
 
-const { log: loggerWithoutType } = declareLogger<'discord' | 'sqlite' | 'rcon'>({ logFilePath: path.resolve('latest.log'), logLevel: import.meta.env.LOG_LEVEL as LogLevel | undefined, serviceColor: {
+const loggerWithoutType = declareLogger<'discord' | 'sqlite' | 'rcon'>({ logLevel: import.meta.env.LOG_LEVEL as LogLevel | undefined, serviceColor: {
   discord: chalk.bold.yellow,
   rcon: chalk.bold.blue,
   sqlite: chalk.bold.magenta,
