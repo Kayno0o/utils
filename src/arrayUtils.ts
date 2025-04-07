acd05f39e1e07bc3f8a677eaaf785e4b76607bec
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-04-07 13:56:52 +0200

    feat(array): shuffle

--- src/arrayUtils.ts
+++ src/arrayUtils.ts
@@ -12,3 +12,10 @@ export function uniqueArray<T extends string | number | symbol | boolean | null 
 export function notEmpty<T>(value: T | null | undefined): value is T {
   return value !== null && value !== undefined
 }
+
+export function shuffleArray(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+}
