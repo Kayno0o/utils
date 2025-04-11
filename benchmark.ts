b02de2b9f223d4544847212cc35caabbefe26950
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-04-11 15:39:58 +0200

    feat(benchmark): remove memory usage

--- benchmark.ts
+++ benchmark.ts
@@ -1,5 +1,4 @@
 import { performance } from 'node:perf_hooks'
-import { heapStats } from 'bun:jsc'
 import { colors, progressBar, randomHex, randomInt, range, searchOne } from './index'
 import { Faker } from './src/faker'
 import { randomString } from './src/text'
@@ -11,54 +10,41 @@ interface BenchmarkOptions {
   warmup?: number
 }
 
-async function warmup(callback: () => any | Promise<any>, duration = 1000) {
+function warmup(callback: () => void, duration = 1000) {
   const end = performance.now() + duration
   while (performance.now() < end) {
-    await callback()
+    callback()
   }
 }
 
-async function benchmark(
+function benchmark(
   name: string,
-  callback: () => any | Promise<any>,
+  callback: () => void,
   { iterations = 1_000, verbose = true, warmup: warmupMs = 1_000 }: BenchmarkOptions,
 ) {
   if (verbose)
     console.log(colors.cyan.bold('[Benchmark]'), name)
 
-  await warmup(callback, warmupMs)
-
-  const memBefore = process.memoryUsage.rss()
-  let peakMem = memBefore
+  warmup(callback, warmupMs)
 
   const startTime = performance.now()
 
   for (let i = 0; i < iterations; i++) {
-    await callback()
-    const mem = process.memoryUsage.rss()
-    if (mem > peakMem)
-      peakMem = mem
+    callback()
   }
 
   const endTime = performance.now()
   const timeTaken = endTime - startTime
-  const memAfter = process.memoryUsage.rss()
-  const heapAfter = heapStats()
 
   if (verbose) {
-    console.log(colors.green('[Time]   '), `${timeTaken.toFixed(2)} ms`)
-    console.log(colors.green('[Memory] '), `${(memAfter / 1024 / 1024).toFixed(2)} MB`)
+    console.log(colors.green('[Time]   '), `${timeTaken.toFixed(10)} ms`)
   }
 
-  return {
-    timeTaken,
-    peakMemoryMB: Math.round(peakMem / 1024 / 1024),
-    finalHeapStats: heapAfter,
-  }
+  return timeTaken
 }
 
-async function suite(
-  benchmarks: [string, () => any | Promise<any>][],
+function suite(
+  benchmarks: [string, () => void][],
   options: BenchmarkOptions = {},
 ) {
   const search = process.argv.slice(2).join(' ')
@@ -69,67 +55,71 @@ async function suite(
   const results: {
     name: string
     timeTaken: number
-    peakMemoryMB: number
   }[] = []
 
-  for (const [name, callback] of benchmarks) {
-    const { timeTaken, peakMemoryMB } = await benchmark(name, callback, options)
-    results.push({ name, timeTaken, peakMemoryMB })
-  }
+  for (const [name, callback] of benchmarks)
+    results.push({ name, timeTaken: benchmark(name, callback, options) })
 
   const maxTime = Math.max(...results.map(result => result.timeTaken))
 
   console.log(colors.magenta.bold('\n[Comparison]'))
 
+  const longestName = results.reduce((acc, curr) => curr.name.length > acc ? curr.name.length : acc, 0)
+
   for (const result of results) {
     const timeColor = [colors.green, colors.red][Number(result.timeTaken === maxTime)]
 
-    console.log(colors.yellow.bold(result.name))
     console.log(
-      colors.blue('[Time]   '),
+      ' '.repeat(longestName - result.name.length) + colors.yellow.bold(result.name),
       progressBar(result.timeTaken, 0, maxTime),
-      timeColor(`${(result.timeTaken / (options.iterations ?? 1_000)).toFixed(3)} ms`),
-    )
-    console.log(
-      colors.blue('[Memory] '),
-      colors.gray(`~${result.peakMemoryMB} MB`),
+      timeColor(`${(result.timeTaken / (options.iterations ?? 1_000)).toFixed(10)} ms`),
     )
   }
 }
 
 console.clear()
 
-await suite([
+suite([
   ['randomInt', () => randomInt(1, 100, false)],
   ['randomInt:crypto', () => randomInt(1, 100, true)],
 ], { iterations: 1_000_000 })
 
-await suite([
+suite([
   ['faker.text:paragraph:50', () => Faker.lorem({ isCrypto: false, length: 50, type: 'paragraph' })],
   ['faker.text:paragraph:50:crypto', () => Faker.lorem({ isCrypto: true, length: 50, type: 'paragraph' })],
 ], { iterations: 1_000 })
 
-await suite([
+suite([
   ['faker.uuidV4', () => Faker.datatype.uuidV4()],
   ['faker.uuidV4:crypto', () => Faker.datatype.uuidV4(true)],
 ], { iterations: 10_000 })
 
-await suite([
+suite([
   ['randomHex', () => randomHex()],
   ['randomHex:crypto', () => randomHex(true)],
 ], { iterations: 1_000_000 })
 
-await suite([
+suite([
   ['range:10', () => range(10)],
   ['range:90-100', () => range(90, 100)],
 ], { iterations: 1_000_000 })
 
 const s = randomString(10_000)
-await suite([
+suite([
   ['colors', () => colors.red([`red`, colors.bold(s)].join(' '))],
   ['colors', () => colors.red([`red`, colors.bold(s)].join(' '))],
   ['colors', () => colors.red(`red ${colors.bold(s)}`)],
   ['colors', () => `${colors.red('red')} ${colors.red.bold(s)}`],
 ], { iterations: 1_000 })
+
+const [r, g, b] = [124, 240, 0]
+suite([
+  ['rgb converted 00001', () => `#${(r << 16 | g << 8 | b).toString(16).padStart(6, '0').toUpperCase()}`],
+  ['rgb converted 2', () => `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`.toUpperCase()],
+  ['rgb converted 3', () => {
+    const hex = (r << 16 | g << 8 | b).toString(16)
+    return `#${('000000'.slice(hex.length) + hex).toUpperCase()}`
+  }],
+], { iterations: 1_000_000 })
 
 console.log()
