dd7223e0fe87c69eb95c9f278ad5520377d10759
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-04-11 11:39:13 +0200

    feat: move methods to appropriate file

--- src/unit.ts
+++ src/unit.ts
@@ -25,3 +25,52 @@ export function getDistance(lat1: number, lon1: number, lat2: number, lon2: numb
   dist *= 1.609344
   return Math.round(dist)
 }
+
+/**
+ * Format bytes as human-readable text.
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ *
+ * @author https://stackoverflow.com/users/65387/mpen
+ * @see https://stackoverflow.com/a/14919494
+ */
+export function getFileSize(bytes: number, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024
+
+  if (Math.abs(bytes) < thresh)
+    return `${bytes} B`
+
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+  let u = -1
+  const r = 10 ** dp
+
+  do {
+    bytes /= thresh
+    ++u
+  } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1)
+
+  return `${bytes.toFixed(dp)} ${units[u]}`
+}
+
+export function formatUnit(num: number): string {
+  if (num < 1000)
+    return num.toString()
+  const units = ['k', 'M', 'B', 'T']
+  let unitIndex = -1
+  let scaledNum = num
+  while (scaledNum >= 1000 && unitIndex < units.length - 1) {
+    scaledNum /= 1000
+    unitIndex++
+  }
+  return `${scaledNum.toFixed(1)}${units[unitIndex]}`
+}
+
+export function formatEuro(amount: number) {
+  return new Intl.NumberFormat('fr-FR', {
+    currency: 'EUR',
+    style: 'currency',
+  }).format(amount)
+}
