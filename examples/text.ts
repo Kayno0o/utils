b711ce0600b2223bc6102f452e921553fea31e03
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-04-15 08:53:15 +0200

    feat: case converter example

---
+++ examples/text.ts
@@ -0,0 +1,8 @@
+import { toCamel, toConstant, toKebab, toPascal, toSnake, toTitle } from '../src/text'
+
+console.log(toPascal('pascalCase')) // PascalCase
+console.log(toCamel('camel-case')) // camelCase
+console.log(toConstant('contant_case')) // CONTANT_CASE
+console.log(toTitle('TitleCase')) // Title Case
+console.log(toKebab('Kebab Case')) // kebab-case
+console.log(toSnake('SNAKE_CASE')) // snake_case
