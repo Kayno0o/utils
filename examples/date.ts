8786868119b9005351ef208c20cb4345123210cc
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-07-31 11:12:34 +0200

    feat: remove dayjs

--- examples/date.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs'
-import { calculateEaster, configureDayjs, formatDate, getFrenchHolidays } from '~'
-
-configureDayjs(dayjs)
-
-console.log(formatDate(dayjs, calculateEaster(2025)))
-console.log(getFrenchHolidays(2025).includes('2025-04-21'))
