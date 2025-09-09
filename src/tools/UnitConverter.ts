56235b2e42a7c227dccb6b4e2f7fb4890fa4ca77
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-09-09 23:27:54 +0200

    feat: add encodePath/decodePath

---
+++ src/tools/UnitConverter.ts
@@ -0,0 +1,197 @@
+export interface UnitType {
+  byte: 'B' | 'kB' | 'MB' | 'GB' | 'TB' | 'PB' | 'EB' | 'ZB' | 'YB'
+  distance: 'm' | 'km' | 'cm' | 'mm' | 'mi' | 'yd' | 'ft' | 'in' | 'nmi'
+  weight: 'g' | 'kg' | 'mg' | 't' | 'lb' | 'oz' | 'st' | 'gr'
+  temperature: 'C' | 'F' | 'K'
+  energy: 'J' | 'kJ' | 'cal' | 'kcal' | 'Wh' | 'kWh' | 'BTU' | 'eV' | 'MJ'
+  pressure: 'Pa' | 'kPa' | 'bar' | 'atm' | 'psi' | 'mmHg' | 'inHg' | 'hPa'
+  speed: 'm/s' | 'km/h' | 'mph' | 'kn' | 'ft/s'
+  time: 'ns' | 'μs' | 'ms' | 's' | 'min' | 'h' | 'd' | 'wk' | 'mo' | 'yr'
+  volume: 'ml' | 'L' | 'm3' | 'cm3' | 'mm3' | 'gal' | 'qt' | 'pt' | 'fl_oz' | 'cup' | 'tbsp' | 'tsp' | 'in3' | 'ft3'
+  area: 'mm2' | 'cm2' | 'm2' | 'ha' | 'km2' | 'in2' | 'ft2' | 'yd2' | 'ac' | 'mi2'
+}
+
+let unitFactors: { [Unit in keyof UnitType]?: Record<UnitType[Unit], number> } = {}
+
+function getUnitFactors() {
+  unitFactors ??= {
+    byte: {
+      B: 1, // base (byte)
+      kB: 1024, // kilobyte
+      MB: 1024 ** 2, // megabyte
+      GB: 1024 ** 3, // gigabyte
+      TB: 1024 ** 4, // terabyte
+      PB: 1024 ** 5, // petabyte
+      EB: 1024 ** 6, // exabyte
+      ZB: 1024 ** 7, // zettabyte
+      YB: 1024 ** 8, // yottabyte
+    },
+    distance: {
+      mm: 0.001, // millimeter
+      cm: 0.01, // centimeter
+      m: 1, // base (meter)
+      km: 1000, // kilometer
+      in: 0.0254, // inch
+      ft: 0.3048, // foot
+      yd: 0.9144, // yard
+      mi: 1609.344, // mile
+      nmi: 1852, // nautical mile
+    },
+    weight: {
+      mg: 0.001, // milligram
+      g: 1, // base (gram)
+      kg: 1000, // kilogram
+      t: 1000000, // metric ton
+      oz: 28.34952, // ounce
+      lb: 453.59237, // pound
+      st: 6350.29318, // stone
+      gr: 0.06479891, // grain
+    },
+    energy: {
+      J: 1, // base (joule)
+      kJ: 1000, // kilojoule
+      MJ: 1000000, // megajoule
+      cal: 4.184, // calorie
+      kcal: 4184, // kilocalorie
+      Wh: 3600, // watt-hour
+      kWh: 3600000, // kilowatt-hour
+      BTU: 1055.06, // British thermal unit
+      eV: 1.602176634e-19, // electronvolt
+    },
+    pressure: {
+      Pa: 1, // base (pascal)
+      kPa: 1000, // kilopascal
+      hPa: 100, // hectopascal
+      bar: 100000, // bar
+      atm: 101325, // standard atmosphere
+      psi: 6894.76, // pounds per square inch
+      mmHg: 133.322, // millimeters of mercury
+      inHg: 3386.39, // inches of mercury
+    },
+    speed: {
+      'm/s': 1, // base (meters per second)
+      'km/h': 0.277778, // kilometers per hour
+      'mph': 0.44704, // miles per hour
+      'kn': 0.514444, // knots
+      'ft/s': 0.3048, // feet per second
+    },
+    time: {
+      ns: 1e-9, // nanosecond
+      μs: 1e-6, // microsecond
+      ms: 0.001, // millisecond
+      s: 1, // base (second)
+      min: 60, // minute
+      h: 3600, // hour
+      d: 86400, // day
+      wk: 604800, // week
+      mo: 2629746, // month (average)
+      yr: 31556952, // year (average)
+    },
+    volume: {
+      mm3: 0.000000001, // cubic millimeter
+      cm3: 0.000001, // cubic centimeter
+      ml: 0.000001, // milliliter (same as cm³)
+      L: 0.001, // liter
+      m3: 1, // base (cubic meter)
+      tsp: 0.00000492892, // teaspoon
+      tbsp: 0.0000147868, // tablespoon
+
+      fl_oz: 0.0000295735, // fluid ounce
+      cup: 0.000236588, // cup
+      pt: 0.000473176, // pint
+      qt: 0.000946353, // quart
+      gal: 0.00378541, // gallon
+      in3: 0.0000163871, // cubic inch
+      ft3: 0.0283168, // cubic foot
+    },
+    area: {
+      mm2: 0.000001, // square millimeter
+      cm2: 0.0001, // square centimeter
+      m2: 1, // base (square meter)
+      ha: 10000, // hectare
+      km2: 1000000, // square kilometer
+      in2: 0.00064516, // square inch
+      ft2: 0.092903, // square foot
+      yd2: 0.836127, // square yard
+      ac: 4046.86, // acre
+      mi2: 2589988.11, // square mile
+    },
+  }
+
+  return unitFactors
+}
+
+let unitConverterFn: {
+  [K in keyof UnitType]?: Record<UnitType[K] & string, {
+    from: (value: number) => number
+    to: (value: number) => number
+  }>
+} = {}
+
+function getUnitConverterFn() {
+  unitConverterFn ??= {
+    byte: createBaseUnitConverters('byte'),
+    distance: createBaseUnitConverters('distance'),
+    weight: createBaseUnitConverters('weight'),
+    energy: createBaseUnitConverters('energy'),
+    pressure: createBaseUnitConverters('pressure'),
+    speed: createBaseUnitConverters('speed'),
+    time: createBaseUnitConverters('time'),
+    volume: createBaseUnitConverters('volume'),
+    area: createBaseUnitConverters('area'),
+    temperature: {
+      C: {
+        from: (value: number) => value + 273.15, // C to K
+        to: (value: number) => value - 273.15, // K to C
+      },
+      F: {
+        from: (value: number) => (value + 459.67) * 5 / 9, // F to K
+        to: (value: number) => value * 9 / 5 - 459.67, // K to F
+      },
+      K: baseUnitConverter(1), // base
+    },
+  }
+
+  return unitConverterFn as {
+    [K in keyof UnitType]: Record<UnitType[K] & string, {
+      from: (value: number) => number
+      to: (value: number) => number
+    }>
+  }
+}
+
+function baseUnitConverter(factor: number) {
+  return {
+    from: (value: number) => value / factor,
+    to: (value: number) => value * factor,
+  }
+}
+
+function createBaseUnitConverters<Unit extends keyof UnitType>(unit: Unit) {
+  const unitFactor = getUnitFactors()[unit] as Record<UnitType[Unit], number>
+  const converters: Record<UnitType[Unit], { from: (value: number) => number, to: (value: number) => number }> = {} as any
+  for (const unit in unitFactor)
+    converters[unit as UnitType[Unit]] = baseUnitConverter(unitFactor[unit as UnitType[Unit]])
+
+  return converters
+}
+
+export class UnitConverter<Unit extends keyof UnitType> {
+  type: Unit
+  unit: UnitType[Unit]
+  value: number
+
+  constructor(type: Unit, unit: UnitType[Unit], value: number) {
+    this.type = type
+    this.unit = unit
+    this.value = getUnitConverterFn()[type][unit].from(value)
+  }
+
+  static from<Unit extends keyof UnitType>(type: Unit, unit: UnitType[Unit], value: number): UnitConverter<Unit> {
+    return new UnitConverter<Unit>(type, unit, value)
+  }
+
+  to(unit: UnitType[Unit]): number {
+    return getUnitConverterFn()[this.type][unit].to(this.value)
+  }
+}
