bebb65620ff91d1b0fa7363ca3d9c0ac896d4bdb
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-10-03 16:21:52 +0200

    build: split cli

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['./index.ts', './i18n/date/*.ts'],
+  entry: ['./index.ts', './cli.ts', './i18n/date/*.ts'],
   format: ['cjs', 'esm'],
   minify: true,
   sourcemap: false,
